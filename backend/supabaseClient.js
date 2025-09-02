f636a60f8599029a22844f4c4b144799e077001e
Sakhi Jindal <sakhi.jindal@gmail.com>
2025-09-02 23:40:43 +0500

    added supabase project url  in frontend and backend both and intalled ldependency.

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://example.supabase.co';
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || 'service-role-key';
+const supabaseUrl = process.env.SUPABASE_URL || 'https://hfijfwmcigiteubbysgo.supabase.co';
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmaWpmd21jaWdpdGV1YmJ5c2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MzYxMDAsImV4cCI6MjA3MjQxMjEwMH0.cDDTHb883jAMFIGFvXRltmGpuNy0Dfxg_ARquAL7KWE';
 
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
