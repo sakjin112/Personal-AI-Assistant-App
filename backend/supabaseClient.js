9c74402f31d087aa3a6d0752b0634e5667cfde49
faisal shahzad <faisal712000@gmail.com>
2025-10-12 03:55:53 +0500

    supabase auth done and tested.

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://hfijfwmcigiteubbysgo.supabase.co';
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmaWpmd21jaWdpdGV1YmJ5c2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MzYxMDAsImV4cCI6MjA3MjQxMjEwMH0.cDDTHb883jAMFIGFvXRltmGpuNy0Dfxg_ARquAL7KWE';
+const supabaseUrl = process.env.SUPABASE_URL || 'https://gegycwfuvdppjgwhovar.supabase.co';
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlZ3ljd2Z1dmRwcGpnd2hvdmFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5NjcxMTIsImV4cCI6MjA3NTU0MzExMn0.HazDFT8MUZJph8j8sUxxiP2NYZvQaVaam1uH0y0Ieo0';
 
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
